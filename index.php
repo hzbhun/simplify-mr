c62e071a9d81c0ef5553753b00f78fc20d2669b6
hzbhun <hegedus.zoltan@ingatlan.com>
2019-11-03 19:50:20 +0100

    script

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+echo "OK";
+ob_flush();
+
+$users = [
+    'hegedus.zoltan' => 'G',
+    'kelemen.gabor' => 'H',
+    'feher.zoltan' => 'E',
+    'hajdu.robert' => 'F'
+];
+
+$params = explode(" ", $_POST['text']);
+$array = [
+    'mr' => $params[0] ?? '',
+    'ticket' => $params[1] ?? '',
+    'desc' => isset($params[2]) ?  implode(' ', array_slice($params, 2)) : '',
+    'owner' => $users[$_POST['user_name']] ?? ''
+];
+
+file_get_contents("https://script.google.com/macros/s//exec?" . http_build_query($array));
+
+$data = array('text' => ':CR: please: ' . $array['mr']);
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/json\r\n",
+        'method'  => 'POST',
+        'content' => json_encode($data),
+    )
+);
+$context = stream_context_create($options);
+file_get_contents('https://hooks.slack.com/services/', false, $context);
